7677dda2916891583a8042aa2ab3a4243df2d90b
wanjiku <ciiku92@gmail.com>
2020-01-13 10:29:55 +0300

    Correct error in app-quote-timeline & app-quote-voting components

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,8 @@ export class Quote {
 
     showQuote: boolean;
 
-    constructor(public id: number, public name: string, public quoteText: string, public quoteAuthor: string, public quoteSubmitter: string, public quoteUpVote: number = 0, public quoteDownVote: number = 0, public dateCreated: Date) {
+    constructor(public id: number, public name: string, public quoteText: string, public quoteAuthor: string,
+        public quoteSubmitter: string, public quoteUpVote: number = 0, public quoteDownVote: number = 0, public dateCreated: Date) {
 
         this.showQuote = false;
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -29,7 +29,7 @@ export class QuoteComponent implements OnInit {
 
   deleteQuote(isComplete, index) {
     if (isComplete) {
-      const toDelete = confirm('Are you sure you want to delete this goal?');
+      const toDelete = confirm('Are you sure you want to delete this quote?');
 
       if (toDelete) {
         this.quotes.splice(index, 1);
